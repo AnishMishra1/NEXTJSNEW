2fdee1008993be08fcfa7ace89fea99ed2607a24
AnishMishra1 <anishmishra143@gmail.com>
2024-05-24 17:42:19 +0530

    adding dyanamic meta tag

---
+++ src/app/metadata/page.js
@@ -0,0 +1,27 @@
+// export default function Metapage(){
+// 	return(
+// 		<div>
+// 			<h1>welcome to meta page</h1>
+// 		</div>
+// 	)
+// }
+
+// export function generateMetaData({params}){
+// 	return {
+// 		title: "meta page",
+// 		description: "usinf to apply dsata on page"
+
+// }
+// }
+export const metadata = {
+    title: "Meta Page",
+    description: "Using to apply data on page",
+};
+
+export default function Metapage() {
+    return (
+        <div>
+            <h1>Welcome to Meta Page</h1>
+        </div>
+    );
+}
