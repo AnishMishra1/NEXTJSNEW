6ff15cf253f05293177b4450bbc9a88d33b9c851
AnishMishra1 <anishmishra143@gmail.com>
2024-05-24 15:51:45 +0530

    adding conditional styling css

---
+++ src/app/csspage/page.js
@@ -0,0 +1,14 @@
+"use client"
+import { useState } from "react"
+import style from "./custom.module.css"
+
+export default function Csspage(){
+	const [color, setColor] = useState("red")
+	return (
+		<div>
+			<h1 className={color =="red" ?style.red: style.green}>heading for css page</h1>
+			<h2 style={{background:color == "red"? style.red : style.green}}>heading 2</h2>
+			<button onClick={() => setColor("green")}>update color</button>
+		</div>
+	)
+}
