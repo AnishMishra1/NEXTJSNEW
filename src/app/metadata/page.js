dd6285ff0184a478442a19469227ef582a0d6780
AnishMishra1 <anishmishra143@gmail.com>
2024-05-24 17:45:33 +0530

    adding dyanamic meta tag through old version n

--- src/app/metadata/page.js
+++ src/app/metadata/page.js
@@ -1,18 +1,4 @@
-// export default function Metapage(){
-// 	return(
-// 		<div>
-// 			<h1>welcome to meta page</h1>
-// 		</div>
-// 	)
-// }
-
-// export function generateMetaData({params}){
-// 	return {
-// 		title: "meta page",
-// 		description: "usinf to apply dsata on page"
-
-// }
-// }
+//new version
 export const metadata = {
     title: "Meta Page",
     description: "Using to apply data on page",
@@ -25,3 +11,26 @@ export default function Metapage() {
         </div>
     );
 }
+
+//old version
+// import Head from 'next/head';
+
+// export default function Metapage() {
+//     return (
+//         <div>
+//             <Head>
+//                 <title>Meta Page</title>
+//                 <meta name="description" content="Using to apply data on page" />
+//             </Head>
+//             <h1>Welcome to Meta Page</h1>
+//         </div>
+//     );
+// }
+
+// export async function generateMetaData({ params }) {
+//     return {
+//         title: "Meta Page",
+//         description: "Using to apply data on page"
+//     };
+// }
+
