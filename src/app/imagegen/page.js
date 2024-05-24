31576925d14440b96e6b69d110a01a5fc65617f0
AnishMishra1 <anishmishra143@gmail.com>
2024-05-24 16:17:33 +0530

    image optimization

---
+++ src/app/imagegen/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Imagegen(){
+	return(
+		<div>
+			<Image src="https://images.pexels.com/photos/60597/dahlia-red-blossom-bloom-60597.jpeg" width={200} height={200}/>
+		</div>
+	)
+}
