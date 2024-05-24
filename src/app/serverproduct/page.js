15506967a8eddb46fabd296519c473ff80f0f9ea
AnishMishra1 <anishmishra143@gmail.com>
2024-05-24 13:18:25 +0530

    conbine server and client rendering

--- src/app/serverproduct/page.js
+++ src/app/serverproduct/page.js
@@ -1,23 +1,26 @@
-async function getProduct(){
-	const response = await fetch("https://jsonplaceholder.typicode.com/users")
-	const data = await response.json()
-	return data
+import Productid from "./productid/page";
+
+//server side render .it use for seo marketing
+async function getProduct() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return data;
 }
 
-export default async function Serverproduct(){
-     let product =  await getProduct()
-     console.log(product)
-      
+export default async function Serverproduct() {
+  let product = await getProduct();
+  console.log(product);
+
   return (
     <div>
-  <h1>Server product rendering</h1>
+      <h1>Server product rendering</h1>
 
-    {
-	product.map((item) => (
-		<h1>{item.name}</h1>
-	))
-    }
+      {product.map((item) => (
+        <div key={item.id}>
+          <h1>{item.name}</h1>
+          <Productid id={item.id} />
+        </div>
+      ))}
     </div>
-  )
+  );
 }
-
---
+++ src/app/serverproduct/productid/page.js
@@ -0,0 +1,9 @@
+"use client"
+
+export default function Productid({id}){
+	return(
+		<div>
+			<button onClick={() => alert(id)}>click me</button>
+		</div>
+	)
+}
