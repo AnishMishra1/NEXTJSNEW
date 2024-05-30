18d5a1e7e7348c0b380a3422fdfc5352a2480d5e
AnishMishra1 <anishmishra143@gmail.com>
2024-05-30 17:46:34 +0530

    laoder added

---
+++ src/app/nextuser/page.js
@@ -0,0 +1,23 @@
+async function userList(){
+    let resposne = await fetch("https://dummyjson.com/users")
+    let data = await resposne.json()
+    return data.users
+}
+
+
+export default async function Page(){
+
+    let users = await userList()
+    console.log("hey i ", users)
+    
+    return (
+        <div>
+            user list printing
+            {
+                users.map( elem => (
+                    <h1>{elem.id}</h1>
+                ))
+            }
+        </div>
+    )
+}
